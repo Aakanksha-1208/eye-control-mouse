ae4569ed6573ddd3d414796564849dcfe7c03ead
Aakanksha-1208 <aakanksha222651@gmail.com>
2025-11-13 00:04:43 +0530

    Add files via upload

---
+++ EYE CONTROL MOUSE.py
@@ -0,0 +1,38 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+
+pyautogui.FAILSAFE = True
+
+
+cam = cv2.VideoCapture(0)
+face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
+scree_w, screen_h = pyautogui.size()
+while True:
+    _, frame = cam.read()
+    frame = cv2.flip(frame, 1)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = face_mesh.process(rgb_frame)
+    Landmarks_points = output.multi_face_landmarks
+    frame_h, frame_w, _ = frame.shape
+    if Landmarks_points:
+        landmarks = Landmarks_points[0].landmark
+        for id , landmark in enumerate(landmarks[474:478], start=474):
+            x = int(landmark.x*frame_w)
+            y = int(landmark.y*frame_h)
+            cv2.circle(frame,(x,y),3,(0,255,0))
+            if id == 475:
+                scree_x=int((landmark.x)*scree_w)
+                scree_y=int((landmark.y)*screen_h)  
+                pyautogui.moveTo(scree_x, scree_y)
+            
+        left =[landmarks[145],landmarks[159]]
+        for landmark in left:
+            x = int(landmark.x*frame_w)
+            y = int(landmark.y*frame_h)
+            cv2.circle(frame,(x,y),3,(0,255,255))
+        if (left[0].y - left[1].y) < 0.025:
+            pyautogui.click()
+            pyautogui.sleep(1)
+    cv2.imshow("eye control mouse", frame)
+    cv2.waitKey(1)
